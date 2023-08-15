641522ea776401b00c8783209ea931540ee1b88e
Akbarali <39323182+akbarali1@users.noreply.github.com>
2023-08-15 21:47:36 +0500

    ad langadd lang

---
+++ uz/auth.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed'   => "Bu hisob ma'lumotlari bizning yozuvlarimizga mos kelmaydi.",
+    'password' => "Taqdim etilgan parol noto'g'ri.",
+    'throttle' => "Kirish urinishlari juda koʻp. Iltimos, :seconds soniyadan keyin qayta urinib ko'ring.",
+
+    'socialite' => [
+        'link_exists'  => 'Hisob allaqachon mavjud',
+        'link_success' => "Hisob bog'langan",
+    ],
+];
---
+++ uz/pagination.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Pagination Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple pagination links. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+
+    'showing' => 'Koʻrsatilmoqda',
+    'of' => 'ning',
+    'to' => 'dan',
+    'results' => 'natijalar',
+    'previous' => '&laquo; Oldingisi',
+    'next' => 'Keyingisi &raquo;',
+
+];
